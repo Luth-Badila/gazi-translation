921345281a4db0f85932b2aa08dd804add78a29e
Luth-Badila <FrancescoLutfi10@gmail.com>
2023-09-07 21:28:18 +0700

    added z-index in waIcon

--- src/app/components/WaIcon.jsx
+++ src/app/components/WaIcon.jsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 
 function WaIcon() {
   return (
-    <div className="fixed bottom-3 right-3 p-3 bg-green-500 rounded-lg">
+    <div className="fixed bottom-3 z-50 right-3 p-3 bg-green-500 rounded-lg">
       <Link href="" className="flex gap-3 items-center">
         <Icon icon="logos:whatsapp-icon" width={40} />
         <p>Hubung kami</p>
